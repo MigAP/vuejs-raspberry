732e93a75f2d0e083730f04687e6b6e81c6dd7f8
Miguel Arpa <miguel.arpa.p@gmail.com>
2018-08-16 17:42:27 +0200

    add even buttons component

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// Gpio checkbox vue component 
+// GPIO CHECKBOX COMPONENTS 
 Vue.component('odd-gpio-check', {
     props: ['button'], 
     template: `
@@ -9,12 +9,27 @@ Vue.component('odd-gpio-check', {
     ` 
 });
 
+Vue.component('even-gpio-check', {
+    props: ['button'], 
+    template: `
+    <div class="even-gpio-check">
+    <label for="button.id" > {{ button.label }} </label> 
+    <input type="checkbox" id="button.id" v-bind:disabled="button.disabled">
+    </div>
+    ` 
+});
+
 let vm = new Vue({
     el: '#app', 
     data: {
-        buttons : [
+        odd_buttons : [
             {id: 1, label: "pepe", disabled: false }, 
             {id: 2, label: "toto", disabled: true}
+        ], 
+
+        even_buttons : [
+            {id: 3, label: "pru", disabled: false }, 
+            {id: 4, label: "tyao", disabled: true}
         ]
     }
 });
